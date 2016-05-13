f6e286de1f3a65e498fdb878ce6ca30f54f2dcb9
Administrator <Administrator@USER-20150923MC>
2016-05-13 16:12:28 +0800

    加入String工具包

--- src/main/java/com/pt/util/StringUtils.java
+++ src/main/java/com/pt/util/StringUtils.java
@@ -2,7 +2,7 @@ package com.pt.util;
 
 import java.util.regex.Pattern;
 
-public class StringUtils {
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
 	
 	/** 
 	* 使用正则表达式 判断是否为整数
@@ -11,6 +11,16 @@ public class StringUtils {
 	*/ 
 	public static boolean isNumberic(String str){ 
 		Pattern p=Pattern.compile("[0-9]*"); 
+		return p.matcher(str).matches(); 
+	} 
+	
+	/** 
+	* 使用正则表达式 判断是否为邮箱
+	* @param str 
+	* @return 
+	*/ 
+	public static boolean isEmail(String str){ 
+		Pattern p=Pattern.compile("\\p{Alpha}\\w{2,15}[@][a-z0-9]{3,}[.]\\p{Lower}{2,}"); 
 		return p.matcher(str).matches(); 
 	} 
 }
